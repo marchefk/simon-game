ab68e1e3ae735fdff21616d3bb65ba75eb1c59ee
marchefk <jackiewicz.ewa@wp.pl>
2017-10-31 22:29:44 +0000

    [4] Works

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var Game = {
-  startNewGame: function(){
+  startNewGame: function() {
     this.soundChain = [];
-    this.userChain = [];
-    this.soundChainIndex = 0;
-    this.userChainIndex = 0;
   },
-  addSound: function(){
+  addSound: function() {
     this.soundChain.push(makeRandomSound());
-  }
+  },
+  playSounds,
 }
 
 var currentGame;
@@ -24,41 +22,112 @@ var sounds = {
   "yellow": audioYellow
 }
 
+var clickedSound;
+var howManyUserSounds = 0;
+var newClick = false;
+
 var newGameButton = $('.button-start');
 newGameButton.on('click', createNewGame);
 
 var soundButtons = $('.sound-button');
 soundButtons.on('click', getSoundFromUser);
 soundButtons.on('click', playClickedButtonSound);
+soundButtons.on('click', stopHighlightingButtons);
+soundButtons.on('click', highlightButtonOnClick);
+soundButtons.on('click', function(){
+  setTimeout(stopHighlightingButtons, 900);    /// to do
+});
+soundButtons.on('click', function(){
+  if(isMatchingSound()){
+    if(checkArraysLength()){
+      howManyUserSounds = 0;
+      currentGame.addSound();
+      setTimeout(function(){currentGame.playSounds();}, 1500);
+    }
+  }else{
+    resetLostGame();
+  }
+}
+);
 
 
 ///// to do:
 
-function isMatchingSound(i){
-  if (currentGame.soundChain[i] == currentGame.userChain[i]){
+function resetLostGame(){
+  currentGame.soundChain = [];
+  howManyUserSounds = 0;
+}
+
+function highlightButton(id){
+  $(`#${id}`).addClass('active');
+}
+
+function highlightButtonOnClick(){
+  highlightButton(this.getAttribute('id'));
+}
+
+function stopHighlightingButtons(){
+  soundButtons.each(function(){
+    if ($(this).hasClass('active')){
+      $(this).removeClass('active')
+    }
+  })
+}
+
+function checkArraysLength(){
+  if (howManyUserSounds == currentGame.soundChain.length){
     return true;
   } else {
     return false;
   }
 }
 
-function playClickedButtonSound(){
-  sounds[this.getAttribute('id')].play();
+function isMatchingSound() {
+  console.log(`comparing ${clickedSound} and ${currentGame.soundChain[howManyUserSounds-1]}`);
+    if (clickedSound == currentGame.soundChain[howManyUserSounds-1]) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
-function getSoundFromUser(){
-  console.log(this.getAttribute('id'));
-  currentGame.userChain.push(this.getAttribute('id'));
+function playSounds() {
+  for (let i = 0; i < currentGame.soundChain.length; i++) {
+    setTimeout(function() {
+      stopHighlightingButtons();
+      sounds[currentGame.soundChain[i]].play();
+      highlightButton(currentGame.soundChain[i]);
+      if (i == currentGame.soundChain.length-1){
+        setTimeout(stopHighlightingButtons, 1000);
+      }
+    }, i * 900);
+  }
 }
 
-function createNewGame(){
+function playClickedButtonSound() {
+  if (currentGame) {
+    sounds[this.getAttribute('id')].play();
+  }
+}
+
+function getSoundFromUser() {
+  if (currentGame) {
+    clickedSound = this.getAttribute('id');
+    howManyUserSounds += 1;
+  }
+}
+
+function createNewGame() {
   var newGame = Object.assign(Object.create(Game));
   newGame.startNewGame();
   currentGame = newGame;
+  newGame.addSound();
+  newGame.playSounds()
+  howManyUserSounds = 0;
 }
 
 function makeRandomSound() {
-  var random = Math.floor(Math.random()*4);
+  var random = Math.floor(Math.random() * 4);
   switch (random) {
     case 0:
       return "blue";
@@ -68,6 +137,7 @@ function makeRandomSound() {
       return "red";
     case 3:
       return "yellow";
-    default: "blue";
+    default:
+      "blue";
   }
 }
